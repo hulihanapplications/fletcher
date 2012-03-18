f035a79ba61e80ed5b658656950154b257ca79ba
Dave Hulihan <dave@hulihanapplications.com>
2012-03-17 20:50:40 -0600

    Updated Factories

--- spec/factories/models.rb
+++ spec/factories/models.rb
@@ -1,39 +1,41 @@
 require "hashie"
 
-Factory.define :base, :class => Fletcher::Model::Base  do |o|
-  o.url "http://www.example.com"  
-end
+FactoryGirl.define do
+  factory :base, :class => Fletcher::Model::Base  do
+    url "http://www.example.com"  
+  end
 
-Factory.define :sample, :parent => :base  do |o|
-  o.url "http://www.example.com"
-  o.name "Sample Model"
-  o.description "This is a sample description."
-  o.raw_price "$5.00"   
-end
+  factory :sample, :parent => :base  do
+    url "http://www.example.com"
+    name "Sample Model"
+    description "This is a sample description."
+    raw_price "$5.00"   
+  end
 
-Factory.define :invalid, :parent => :base do |o|
-  o.url "http://invalid"
-end  
+  factory :invalid, :parent => :base do
+    url "http://invalid"
+  end  
 
-Factory.define :valid, :parent => :base do |o|
-  o.url "http://www.amazon.com/Kindle-Fire-Amazon-Tablet/dp/B0051VVOB2"
-  o.title_xpath "//h1[@class='parseasinTitle']"
-  o.images_xpath "//table[@class='productImageGrid']//img"
-end
+  factory :valid, :parent => :base do
+    url "http://www.amazon.com/Kindle-Fire-Amazon-Tablet/dp/B0051VVOB2"
+    title_xpath "//h1[@class='parseasinTitle']"
+    images_xpath "//table[@class='productImageGrid']//img"
+  end
 
-Factory.define :amazon, :parent => :base do |o|
-  o.url "http://www.amazon.com/Kindle-Fire-Amazon-Tablet/dp/B0051VVOB2"
-end
+  factory :amazon, :parent => :base do
+    url "http://www.amazon.com/Kindle-Fire-Amazon-Tablet/dp/B0051VVOB2"
+  end
 
-# This may need to be updated if the item is expired/deleted
-Factory.define :ebay, :parent => :base do |o|
-  o.url "http://www.ebay.com/itm/Franklin-Library-3V-ESSAYS-MONTAIGNE-Leather-Great-Books-1979-Donald-Frame-/190654183378"
-end
+  # This may need to be updated if the item is expired/deleted
+  factory :ebay, :parent => :base do
+    url "http://www.ebay.com/itm/Franklin-Library-3V-ESSAYS-MONTAIGNE-Leather-Great-Books-1979-Donald-Frame-/190654183378"
+  end
 
-Factory.define :thinkgeek, :parent => :base do |o|
-  o.url "http://www.thinkgeek.com/geektoys/plush/e7f8/"
-end
+  factory :thinkgeek, :parent => :base do
+    url "http://www.thinkgeek.com/geektoys/plush/e7f8/"
+  end
 
-Factory.define :etsy, :parent => :base do |o|
-  o.url "http://www.etsy.com/listing/78608690/farm-fresh-125-x-19-in-pick-your-color"
+  factory :etsy, :parent => :base do
+    url "http://www.etsy.com/listing/78608690/farm-fresh-125-x-19-in-pick-your-color"
+  end
 end
