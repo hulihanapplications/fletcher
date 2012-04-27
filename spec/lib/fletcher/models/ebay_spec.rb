a9153f34f0e4c341685b566418509d05b03f7e6b
Dave Hulihan <dave@hulihanapplications.com>
2012-04-27 16:49:53 -0600

    Disabled ebay realtime test

--- spec/lib/fletcher/models/ebay_spec.rb
+++ spec/lib/fletcher/models/ebay_spec.rb
@@ -3,13 +3,14 @@ require 'spec_helper'
 describe Fletcher::Model::Ebay do
   describe "parse" do     
     context "with valid data" do
-      it "should return correct model info" do 
-        model = described_class.new
-        model.parse Fletcher::Data.read(Factory(:ebay).url)
-        model.name.should_not be_nil
-        model.price.should_not be_nil        
-        model.image.should_not be_nil        
-      end       
+      # Diable Realtime Test, too inconsistent
+      # it "should return correct model info" do 
+      #   model = described_class.new
+      #   model.parse Fletcher::Data.read(Factory(:ebay).url)
+      #   model.name.should_not be_nil
+      #   model.price.should_not be_nil        
+      #   model.image.should_not be_nil        
+      # end       
     end
   end
 end  
