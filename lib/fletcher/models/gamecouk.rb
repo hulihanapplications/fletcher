9cce633c573840574319d69551457f9f97796716
David Collom <david.collom@cef.co.uk>
2012-08-17 14:28:26 +0100

    Fix Travis - invalid multibyte char (US-ASCII)

--- lib/fletcher/models/gamecouk.rb
+++ lib/fletcher/models/gamecouk.rb
@@ -27,7 +27,7 @@ module Fletcher
           
           # Get Price
           raw_price = doc.css("ul#variants ul.mint li.price").first_string
-          parse_price(raw_price.gsub(/Only £/, ""),:GBP) if raw_price
+          parse_price(raw_price.gsub(/Only /, ""),:GBP) if raw_price
           
           # Get Image
           self.images = [{:url => doc.xpath("//meta[@property='og:image']/@content").first_string}]
