88369dd88cf629e2c3dfbc9515517ff0efbc9cd6
lumir <lumir.olivares@koombea.com>
2014-05-05 09:51:57 -0500

    adding user_agent as parameter to the data.read method

--- lib/fletcher/data.rb
+++ lib/fletcher/data.rb
@@ -5,15 +5,16 @@ module Fletcher
   # This class is responsible for fetching and parsing data.
   class Data
     # Get read url and get data object
-    def self.read(url, options = {})
-      response = open(url, "User-Agent" => Fletcher::USER_AGENT)
+    def self.read(url, agent = nil)
+      user_agent = agent.blank? ? Fletcher::USER_AGENT : agent
+      response = open(url, "User-Agent" => user_agent)
       doc = ::Nokogiri::HTML(response.read)
-      
+
       # Save contents of URL/Remote File for debugging
       # response.rewind
-      # last_response_file = File.expand_path(File.join("..", "..", "last_response"), File.dirname(__FILE__))        
+      # last_response_file = File.expand_path(File.join("..", "..", "last_response"), File.dirname(__FILE__))
       # File.new(last_response_file, "w+").write(response.read)
       return doc
-    end 
+    end
   end
 end
